638dcffbf3d0a5e61f8c634da1e5fff2e20f1fda
Drazen Popovic <bladyjoker@gmail.com>
2022-08-08 15:39:32 +0200

    Scaffold the PAB Cli `oracle-pab-cli deploy`

---
+++ oracle-pab/app/Cardano/Oracle/Cli/Deploy.hs
@@ -0,0 +1,14 @@
+module Cardano.Oracle.Cli.Deploy (DeployOpts (..), DeployMode (..), deploy) where
+
+data DeployMode = DEPLOY_PROD | DEPLOY_DEBUG deriving stock (Show, Read, Eq)
+
+data DeployOpts = DeployOpts
+  { do'Mode :: DeployMode
+  , do'PlutusFile :: FilePath
+  , do'DeploymentFile :: FilePath
+  }
+  deriving stock (Show, Eq)
+
+deploy :: DeployOpts -> IO ()
+deploy opts = do
+  return ()
--- oracle-pab/app/Main.hs
+++ oracle-pab/app/Main.hs
@@ -1,4 +1,72 @@
-module Main where
+module Main (main) where
+
+import Cardano.Oracle.Cli.Deploy (DeployMode (DEPLOY_DEBUG), DeployOpts (DeployOpts), deploy)
+
+import Control.Applicative ((<**>))
+import Options.Applicative (
+  Parser,
+  ParserInfo,
+  auto,
+  command,
+  customExecParser,
+  fullDesc,
+  help,
+  helper,
+  info,
+  long,
+  metavar,
+  option,
+  prefs,
+  progDesc,
+  showDefault,
+  showHelpOnEmpty,
+  showHelpOnError,
+  strOption,
+  subparser,
+  value,
+ )
+
+newtype Command
+  = Deploy DeployOpts
+
+deployOpts :: Parser DeployOpts
+deployOpts =
+  DeployOpts
+    <$> option
+      auto
+      ( long "mode"
+          <> metavar "DEPLOY_MODE"
+          <> help "Mode of deployment DEPLOY_DEBUG|DEPLOY_PROD"
+          <> value DEPLOY_DEBUG
+          <> showDefault
+      )
+    <*> strOption
+      ( long "plutus-file"
+          <> metavar "PLUTUS_FILE"
+          <> help "A JSON file to read the compiled COOP Plutus scripts from"
+          <> value "coop-plutus.json"
+          <> showDefault
+      )
+    <*> strOption
+      ( long "deployment-file"
+          <> metavar "DEPLOYMENT_FILE"
+          <> help "A JSON file to write the deployment information to"
+          <> value "coop-deployment.json"
+          <> showDefault
+      )
+
+options :: Parser Command
+options =
+  subparser $
+    command
+      "deploy"
+      (info (Deploy <$> deployOpts <* helper) (progDesc "Deploy COOP on the Cardano network and write the deployment information a file"))
+
+parserInfo :: ParserInfo Command
+parserInfo = info (options <**> helper) (fullDesc <> progDesc "COOP PAB cli tools")
 
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  cmd <- customExecParser (prefs (showHelpOnEmpty <> showHelpOnError)) parserInfo
+  case cmd of
+    Deploy opts -> deploy opts
